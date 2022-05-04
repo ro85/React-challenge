592e04e9a0d91dac434e8349932bb817a21f94f8
Rodrigo Villanueva <r_villanueva18@hotmail.com>
2022-05-04 13:27:02 -0300

    Fetch Api index

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+  
 
-function App() {
+const App = () => {
+
+  const [drinks, setDrinks] = useState([]);
+
+  const url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass"
+  
+  const fetchDrinks = (url) => {
+    fetch(url)
+      .then(response => response.json())    
+      .then(data => setDrinks(data.drinks))     
+      .catch(error => console.log(error))    
+  };
+
+  useEffect(() =>{
+    fetchDrinks(url);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
