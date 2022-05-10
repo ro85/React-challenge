4c994747c9ab537a09a6dbd4efba0254085a4632
Rodrigo Villanueva <r_villanueva18@hotmail.com>
2022-05-10 10:59:23 -0300

    Change ingredients for ingredientsAndMeasures in DrinkPage

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ const App = () => {
   
   const fetchDrinks = (url) => {
     fetch(url)
-      .then(response => response.json())    
-      .then(data => setDrinks(data.drinks))     
-      .catch(error => console.log(error))    
+    .then(response => response.json())    
+    .then(data => setDrinks(data.drinks))     
+    .catch(error => console.log(error))    
   };
 
   useEffect(() =>{
--- src/pages/DrinkPage.jsx
+++ src/pages/DrinkPage.jsx
@@ -8,7 +8,7 @@ const DrinkPage = ({drinkId}) => {
   const navigate = useNavigate();
   const { idDrink } = useParams();
   const [drink, setDrink] = useState({});
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredientsAndMeasures, setIngredientsAndMeasures] = useState([]);
 
   const urlDrink = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`
   
@@ -23,10 +23,10 @@ const DrinkPage = ({drinkId}) => {
           instruction: data.drinks[0].strInstructions
         };
   
-        const ingredientsInfo = [`${data.drinks[0].strMeasure1} - ${data.drinks[0].strIngredient1}` , `${data.drinks[0].strMeasure2} - ${data.drinks[0].strIngredient2}`, `${data.drinks[0].strMeasure3} - ${data.drinks[0].strIngredient3}`, `${data.drinks[0].strMeasure4} - ${data.drinks[0].strIngredient4}`, `${data.drinks[0].strMeasure5} - ${data.drinks[0].strIngredient5}`, `${data.drinks[0].strMeasure6} - ${data.drinks[0].strIngredient6}`, `${data.drinks[0].strMeasure7} - ${data.drinks[0].strIngredient7}`, `${data.drinks[0].strMeasure8} - ${data.drinks[0].strIngredient8}`, `${data.drinks[0].strMeasure9} - ${data.drinks[0].strIngredient9}`, `${data.drinks[0].strMeasure10} - ${data.drinks[0].strIngredient10}`, `${data.drinks[0].strMeasure11} - ${data.drinks[0].strIngredient11}`, `${data.drinks[0].strMeasure12} - ${data.drinks[0].strIngredient12}`, `${data.drinks[0].strMeasure13} - ${data.drinks[0].strIngredient13}`, `${data.drinks[0].strMeasure14} - ${data.drinks[0].strIngredient14}`, `${data.drinks[0].strMeasure15} - ${data.drinks[0].strIngredient15 }` ]       
+        const ingredientsAndMeasuresInfo = [`${data.drinks[0].strMeasure1} - ${data.drinks[0].strIngredient1}` , `${data.drinks[0].strMeasure2} - ${data.drinks[0].strIngredient2}`, `${data.drinks[0].strMeasure3} - ${data.drinks[0].strIngredient3}`, `${data.drinks[0].strMeasure4} - ${data.drinks[0].strIngredient4}`, `${data.drinks[0].strMeasure5} - ${data.drinks[0].strIngredient5}`, `${data.drinks[0].strMeasure6} - ${data.drinks[0].strIngredient6}`, `${data.drinks[0].strMeasure7} - ${data.drinks[0].strIngredient7}`, `${data.drinks[0].strMeasure8} - ${data.drinks[0].strIngredient8}`, `${data.drinks[0].strMeasure9} - ${data.drinks[0].strIngredient9}`, `${data.drinks[0].strMeasure10} - ${data.drinks[0].strIngredient10}`, `${data.drinks[0].strMeasure11} - ${data.drinks[0].strIngredient11}`, `${data.drinks[0].strMeasure12} - ${data.drinks[0].strIngredient12}`, `${data.drinks[0].strMeasure13} - ${data.drinks[0].strIngredient13}`, `${data.drinks[0].strMeasure14} - ${data.drinks[0].strIngredient14}`, `${data.drinks[0].strMeasure15} - ${data.drinks[0].strIngredient15 }` ]       
   
         setDrink(drinkInfo);
-        setIngredients(ingredientsInfo);        
+        setIngredientsAndMeasures(ingredientsAndMeasuresInfo);        
         
       })   
       .catch(error => console.log(error))    
@@ -38,7 +38,7 @@ const DrinkPage = ({drinkId}) => {
 
   return (       
     <div className='d-flex justify-content-center'>        
-      <DrinkRecipe drinkId={idDrink} drink={drink} ingredients={ingredients} />      
+      <DrinkRecipe drinkId={idDrink} drink={drink} ingredients={ingredientsAndMeasures} />      
     </div> 
   )
 }
